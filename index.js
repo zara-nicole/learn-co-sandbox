2d4170dce9787574af149528034b7b28babdc896
zara.nicole3 <zara.nicole3@aol.com>
2019-01-02 12:39:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+// declare the variable
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
+console.log(window.word) // 'dog'
